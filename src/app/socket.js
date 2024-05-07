a9396e0c1a46d8b5a3643b6e28d4081df39e639e
maxim12313 <maximkim16@gmail.com>
2024-05-07 00:09:05 -0700

    separating backend and front end

--- src/app/socket.js
+++ src/app/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const development = "http://localhost8080";
-const production = "http://localhost8080";
+const development = "http://localhost:8080";
+const production = "http://localhost:8080";
 const backEnd = process.env.NODE_ENV == "development" ? development : production;
 
 export const socket = io(
