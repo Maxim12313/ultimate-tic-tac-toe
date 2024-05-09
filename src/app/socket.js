71eae40c561443855b959ab0a3e94a62bf442312
maxim12313 <maximkim16@gmail.com>
2024-05-08 17:38:13 -0700

    updated socket backend production url

--- src/app/socket.js
+++ src/app/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const development = "http://localhost:8080";
-const production = "http://localhost:8080";
+const production = "https://ultimate-tic-tac-toe-backend-hmitsogtdq-ue.a.run.app";
 const backEnd = process.env.NODE_ENV == "development" ? development : production;
 
 export const socket = io(
